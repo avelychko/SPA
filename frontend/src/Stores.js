6c09b363b1aa41a1eaa33f4e28fba0e94940573a
Carlos Escalante <cescalante@mines.edu>
2023-04-12 16:50:46 -0400

    added a test store

--- frontend/src/Stores.js
+++ frontend/src/Stores.js
@@ -5,16 +5,16 @@ export default function Stores() {
   
     return (
       <>
-        {/* <link rel="stylesheet" href="Stores.css"/> */}
+        <link rel="stylesheet" href="Stores.css"/>
         <h1>My stores</h1>
-          {/*stores.map((store) => (
-            <div key={store.id}>
+          {[{ id: 1, name: "Target"}].map((store) => (
+            <div key={store.id} className="card">
                 <Link to={`${store.id}`}><h1 className="label-font">{store.name}</h1></Link>
                 <h4>Products: {// todo 
                 }</h4>
-                <h4>ID: { store.id }</h4>
+                <h4 className="label-font">ID: { store.id }</h4>
           </div>
-              ))*/}
+              ))}
 
       </>
     );
